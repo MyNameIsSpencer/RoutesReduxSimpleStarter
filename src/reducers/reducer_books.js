b31a5ff7b40dbed25fdaf53882df040cea2ad51a
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-05-16 02:37:43 -0400

    lecture 48

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: "Mama's House"},
+    {title: "Jamboree"},
+    {title: "Whatcinya Cookin?"},
+    {title: "Vivivia"},
+    {title: "Unbecomest "}
+  ]
+}
